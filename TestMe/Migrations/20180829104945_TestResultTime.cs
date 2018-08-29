b2f1b71b9008748858e854fe61173d7a652adda2
Вадим Матырин <maturin030700@gmail.com>
2018-08-29 13:51:30 +0300

    Add start and finish time to the TestResult model

---
+++ TestMe/Migrations/20180829104945_TestResultTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestMe.Migrations
+{
+    public partial class TestResultTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "TestResults",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinishTime",
+                table: "TestResults",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "TestResults",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinishTime",
+                table: "TestResults");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "TestResults");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "TestResults",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- TestMe/Models/TestResult.cs
+++ TestMe/Models/TestResult.cs
@@ -11,9 +11,13 @@ namespace TestMe.Models
         public int Id { get; set; }
         [Required]
         public int Score { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime FinishTime { get; set; }
         public int TestId { get; set; }
-
         public Test Test;
         //public string AppUserId { get; set; }
         //public AppUser AppUser { get; set; }
