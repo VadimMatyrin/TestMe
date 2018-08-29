7008234ee351c4784886df0322d549d9dce2859b
Вадим Матырин <maturin030700@gmail.com>
2018-08-29 15:16:51 +0300

    Add test duration management. User test results display

--- TestMe/Models/Test.cs
+++ TestMe/Models/Test.cs
@@ -12,14 +12,20 @@ namespace TestMe.Models
         [JsonIgnore]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Test name")]
         [StringLength(200)]
         public string TestName { get; set; }
         [JsonIgnore]
+        [Display(Name = "Creation date")]
         public DateTime CreationDate { get; set; }
         [JsonIgnore]
         public string AppUserId { get; set; }
         public string TestCode { get; set; }
-        public TimeSpan TestDuration {get;set;}
+        [Display(Name = "Test duration")]
+        [Required, DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh\\:mm}")]
+        [RegularExpression(@"((([0-1][0-9])|(2[0-3]))(:[0-5][0-9])(:[0-5][0-9])?)", ErrorMessage = "Duration must be between 00:00 to 23:59")]
+        public TimeSpan TestDuration { get; set; }
         [JsonIgnore]
         public AppUser AppUser { get; set; }
         [JsonIgnore]
