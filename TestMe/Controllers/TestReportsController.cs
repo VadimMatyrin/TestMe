89ceb91332924b03ebb90f1ba8c5efffc09a9d9c
Вадим Матырин <maturin121176@yandex.ru>
2018-09-05 13:42:52 +0300

    Add a possibility to report a test

--- TestMe/Controllers/TestReportsController.cs
+++ TestMe/Controllers/TestReportsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using TestMe.Models;
 using TestMe.Sevices.Interfaces;
 
@@ -15,10 +16,15 @@ namespace TestMe.Controllers
     {
         private readonly ITestingPlatform _testingPlatform;
         private readonly UserManager<AppUser> _userManager;
+        private string _userId;
         public TestReportsController(ITestingPlatform testingPlatform, UserManager<AppUser> userManager)
         {
             _testingPlatform = testingPlatform;
             _userManager = userManager;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        { 
+            _userId = _userManager.GetUserId(User);
         }
         public IActionResult Index(int? id)
         {
@@ -29,6 +35,39 @@ namespace TestMe.Controllers
                 return NotFound();
 
             return View(test);
+        }
+        public async Task<IActionResult> Create(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var test = await _testingPlatform.TestManager.FindAsync(t => t.AppUserId == _userId && t.Id == id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+
+            var testAnswer = new TestReport { Test = test, TestId = test.Id };
+            return View(testAnswer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Message, TestId")] TestReport testReport)
+        {
+            if (ModelState.IsValid)
+            {
+                testReport.AppUserId = _userId;
+                var test = await _testingPlatform.TestManager.FindAsync(t => t.Id == testReport.TestId);
+                if (test is null)
+                    return View(testReport);
+
+                await _testingPlatform.TestReportManager.AddAsync(testReport);
+                return RedirectToAction(nameof(Index), "TestEngine", new { code = test.TestCode});
+            }
+            return View(testReport);
         }
     }
 }
