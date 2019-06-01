312947d56d2139a695a3bc96e0d60a29e3a29ba2
Вадим Матырин <maturin030700@gmail.com>
2019-06-01 10:57:02 +0300

    Add new login page

--- TestMe/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TestMe/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -14,103 +14,11 @@ using TestMe.Models;
 namespace TestMe.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
-    public class LoginModel : PageModel
+    public class LoginModelOld : PageModel
     {
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
-        private readonly UserManager<AppUser> _userManager;
-
-        public LoginModel(SignInManager<AppUser> signInManager, ILogger<LoginModel> logger, UserManager<AppUser> userManager)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
-            _signInManager = signInManager;
-            _logger = logger;
-            _userManager = userManager;
-        }
-
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        public string ReturnUrl { get; set; }
-
-        [TempData]
-        public string ErrorMessage { get; set; }
-
-        public class InputModel
-        {
-            [Required]
-            public string Username { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
-        }
-
-        public async Task OnGetAsync(string returnUrl = null)
-        {
-            if (!string.IsNullOrEmpty(ErrorMessage))
-            {
-                ModelState.AddModelError(string.Empty, ErrorMessage);
-            }
-
-            returnUrl = returnUrl ?? Url.Content("~/");
-
-            // Clear the existing external cookie to ensure a clean login process
-            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            ReturnUrl = returnUrl;
-        }
-
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl = returnUrl ?? Url.Content("~/");
-
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var user = await _userManager.FindByNameAsync(Input.Username);
-                if(user is null)
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attampt");
-                    return Page();
-                }
-                if (user.IsBanned && (await _signInManager.CheckPasswordSignInAsync(user, Input.Password, false)).Succeeded)
-                {
-                    await _signInManager.SignOutAsync();
-                    ModelState.AddModelError(string.Empty, "Your account was banned");
-                    return Page();
-                }
-                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: true);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return Page();
+            return RedirectToAction("Register", "Identity/Account");
         }
     }
 }
