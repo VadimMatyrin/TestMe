faa32a61f04d9eec0c54709095c108690e5a37a6
Вадим Матырин <maturin030700@gmail.com>
2019-06-01 13:21:07 +0300

    Add bool RequiresTeacherToStart field to Test model

---
+++ TestMe/Migrations/20190601102032_AddRequiresTeacherFieldToTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestMe.Migrations
+{
+    public partial class AddRequiresTeacherFieldToTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresTeacherToStart",
+                table: "Tests",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequiresTeacherToStart",
+                table: "Tests");
+        }
+    }
+}
